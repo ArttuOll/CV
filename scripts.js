953b5212bfb731d3e1c89a6ce7f874ae9f37df40
ArttuOll <60509537+ArttuOll@users.noreply.github.com>
2024-12-27 16:25:43 +0200

    Add dark mode

---
+++ scripts.js
@@ -0,0 +1,19 @@
+﻿const colorScheme = document.querySelector("meta[name=color-scheme]");
+const switchButtons = document.querySelectorAll("button");
+
+colorScheme.content = localStorage.getItem("theme");
+
+switchButtons.forEach((switchButton) => {
+  switchButton.addEventListener("click", onThemeToggleClick);
+});
+
+function onThemeToggleClick(event) {
+  const currentButton = event.target;
+
+  switchButtons.forEach((button) =>
+    button.setAttribute("aria-pressed", button === currentButton),
+  );
+
+  colorScheme.content = currentButton.value;
+  localStorage.setItem("theme", currentButton.value);
+}
